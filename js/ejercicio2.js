14835d8ada17ac725eb0592a62e2849f048cf273
Javier Gonzalez <javiergonzalezupvm@gmail.com>
2023-07-06 08:18:32 -0600

    Ejercciios2

---
+++ js/ejercicio2.js
@@ -0,0 +1,23 @@
+/*El nombre del objeto ha sido cambiado por "Repair droid" y su precio se ha 
+incrementado en 1500 créditos. Anule los valores de las variables pricePerItem y productName 
+después de su declaración.*/
+
+/*
+let pricePerItem = 2000;
+pricePerItem = 3500;
+
+let producName = "Droid";
+producName = "Repair droid";
+
+console.log(pricePerItem);
+console.log(producName);
+*/
+let pricePerItem = 3000;
+pricePerItem = 4500;
+
+let producName = "Videogame";
+producName = "Add Videogame";
+
+console.log(pricePerItem);
+console.log(producName);
+
