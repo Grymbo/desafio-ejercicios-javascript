770210a9c849bc25d50ff0c31d5c3c0d3ac7c691
Javier Gonzalez <javiergonzalezupvm@gmail.com>
2023-07-06 08:21:38 -0600

    2

--- js/ejercicio1.js
+++ js/ejercicio1.js
@@ -2,8 +2,15 @@
 el precio por articulo. Al declarar las variables, se debe de asignar las siguientes caracteriticas 
 del producto alas variables.*/
 
+/*
 const producName = "Droid";
 const pricePerItem = 2000;
 
 console.log(producName);
 console.log(pricePerItem);
+*/
+const videoGameName = "Spiderman";
+const pricePerVideoGame = 665;
+
+console.log(videoGameName);
+console.log(pricePerVideoGame);
