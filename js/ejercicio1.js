2eecdc944144dfd3b6da244ce12ee2809c2c11e4
Javier Gonzalez <javiergonzalezupvm@gmail.com>
2023-07-06 08:07:24 -0600

    Ejercicio1

---
+++ js/ejercicio1.js
@@ -0,0 +1,9 @@
+/*Declare dos variables, producName para el nombre del producto y pricePerItem para almacenar 
+el precio por articulo. Al declarar las variables, se debe de asignar las siguientes caracteriticas 
+del producto alas variables.*/
+
+const producName = "Droid";
+const pricePerItem = 2000;
+
+console.log(producName);
+console.log(pricePerItem);
