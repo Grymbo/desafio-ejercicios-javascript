ff0abc8c6b40b09947c6cf866b0af802ddc576c2
Javier Gonzalez <javiergonzalezupvm@gmail.com>
2023-07-07 19:39:01 -0600

    3

---
+++ ejercicio3.js
@@ -0,0 +1,15 @@
+/*
+Declare las siguientes variables usando la palabra clave const o let y asígneles los valores adecuados.
+
+topSpeed - número 160.
+distance - número 617.54.
+login - secuencia "mango935".
+isOnline - boolean true.
+isAdmin - boolean false.
+*/
+const topSpeed = 160;
+const distance = 617.54;
+
+let login = "mango935";
+let isOnline = true;
+let isAdmin = false;
