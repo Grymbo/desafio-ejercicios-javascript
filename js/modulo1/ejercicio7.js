fa59f81774a3982c75ad673b21f6aea752bcbfe1
Javier Gonzalez <javiergonzalezupvm@gmail.com>
2023-07-08 21:10:42 -0600

    7

---
+++ js/modulo1/ejercicio7.js
@@ -0,0 +1,22 @@
+/*
+Declara la función sayHi, dentro de la cual añade console.log() con la secuencia "Hello, 
+this is my first function!". Después de la declaración, la llamada de función sayHi.
+
+Se espera la declaración de la función
+La función se denomina sayHi
+En el cuerpo de la función sayHi hay console.log("Hello, this is my first function!")
+Después de la declaración hay una llamada de la función sayHi
+*/
+
+function sayHi() {
+    console.log("Hello, this is my first function");
+}
+
+sayHi();
+
+
+function videoGame() {
+    console.log("Usted compro muchos videojuegos que ni juega jajajaja");
+}
+
+videoGame();
