27e6dfcec301b7e77d77240295d18e3d3ecd526c
Javier Gonzalez <javiergonzalezupvm@gmail.com>
2023-07-17 19:35:42 -0600

    15, 16, 17, 18

---
+++ js/modulo1/ejercicio15.js
@@ -0,0 +1,26 @@
+/**
+ * Añadir una expresión para comprobar la edad del usuario, el valor de age, en la condición para 
+ * la instrucción if.
+
+Si el usuario es un adulto, el bloque if debe ejecutarse y en la variable message se escribe la 
+secuencua "You are an adult".
+En caso contrario, se debe ejecutar el bloque else' y escribir la secuencia "You are a minor"`.
+Declare la función checkAge(age).
+La expresión de verificación usada por el operador >=
+Al llamar a checkAge(20) devuelve "You are an adult"
+Al llamar a checkAge(8) devuelve "You are a minor"
+Al llamar a checkAge(14) devuelve "You are a minor"
+Al llamar a checkAge(38) devuelve "You are an adult"
+ */
+function checkAge(age) {
+    if (age >= 18) {
+        return "You are and adult";
+    } else {
+        return "You are a minor";
+    }
+}
+
+console.log(checkAge(20));
+console.log(checkAge(8));
+console.log(checkAge(14));
+console.log(checkAge(38));
---
+++ js/modulo1/ejercicio17.js
@@ -0,0 +1,25 @@
+/**
+ * Sustituir las expresiones con operadores matemáticos estándar por una combinación de operador de 
+ * asignación con suma, resta, multiplicación y división.
+
+El valor de la variable a es igual a 7
+Operador empleado +=
+El valor de la variable b es igual a 6
+Operador empleado -=
+El valor de la variable c es igual a 45
+Operador empleado *=
+El valor de la variable d es igual a 2
+Operador empleado /=
+ */
+
+let a = 5;
+let b = 10;
+let c = 15;
+let d = 20;
+
+a += 7;
+b -= 4;
+c *= 3;
+d /= 10;
+
+console.log(a, b, c, d);
